8a0f2639ce0ce24be8cfb04d21f0001ad09e5285
pewww <jung22337@naver.com>
2020-12-12 19:08:05 +0900

    :lipstick: 고담 폰트 추가 및 GlobalStyle에 font-face 적용

--- pewww-wil-page/src/styles/global.styles.ts
+++ pewww-wil-page/src/styles/global.styles.ts
@@ -1,6 +1,28 @@
 import {createGlobalStyle} from 'styled-components';
 
+const gothamBoldOtf = require('../assets/fonts/Gotham-Bold.otf');
+const gothamMediumOtf = require('../assets/fonts/Gotham-Medium.otf');
+const gothamLightOtf = require('../assets/fonts/Gotham-Light.otf');
+
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "Gotham Bold";
+    src: url("${gothamBoldOtf}") format('opentype');
+    font-weight: 600;
+  }
+
+  @font-face {
+    font-family: "Gotham Medium";
+    src: url("${gothamMediumOtf}") format('opentype');
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: "Gotham Light";
+    src: url("${gothamLightOtf}") format('opentype');
+    font-weight: 300;
+  }
+
   html, body {
     width: 100%;
     height: 100%;
